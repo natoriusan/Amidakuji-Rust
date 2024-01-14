068bfe066ee36d1c19d62af34319cc653bb4b63a
natoriusan <58880731+natoriusan@users.noreply.github.com>
2024-01-14 23:12:46 +0900

    Change function call

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use crate::functions::*;
 mod functions;
 
 fn main() {
-    let results = construct_connected_amidakuji_with_long_line().calculate_parallel(3..=500);
+    let results = construct_connected_amidakuji_with_long_line().calculate_parallel(3..=40);
     results.output_to_file("output/amidakuji.csv");
 }
